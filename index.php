564a5a086891b9a0bc23af8ada55107e72536a29
rui95fer <rui95fer@gmail.com>
2025-07-06 11:19:31 +0100

    Your First PHP Tag

---
+++ index.php
@@ -0,0 +1,15 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Laracasts: PHP for Begginers</title>
+</head>
+<body>
+<h1>
+    <?php
+        echo "Welcome to Laracasts: PHP for Beginners!";
+    ?>
+</h1>
+</body>
+</html>
