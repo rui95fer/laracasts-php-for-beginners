fb1729dc5171a030308e94ae5faa2f83d51e8c7d
rui95fer <rui95fer@gmail.com>
2025-07-14 17:53:35 +0100

    Conditionals and Booleans

--- index.php
+++ index.php
@@ -3,14 +3,22 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Laracasts: PHP for Begginers</title>
+    <title>Laracasts: PHP for Beginners</title>
 </head>
 <body>
+    <?php
+        $book = "Learning PHP, MySQL & JavaScript";
+        $haveRead = true;
+
+        if ($haveRead) {
+            $message = "I have read the book: $book.";
+        } else {
+            $message = "I have not read the book: $book.";
+        }
+    ?>
 <h1>
     <?php
-        $course = "PHP for Beginners";
-
-        echo "Welcome to Laracasts: $course!";
+        echo $message;
     ?>
 </h1>
 </body>
