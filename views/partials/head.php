7efe648a96d938afcf4634ea14ad21a7fb534355
rui95fer <rui95fer@gmail.com>
2025-07-18 13:01:48 +0100

    PHP Partials

---
+++ views/partials/head.php
@@ -0,0 +1,9 @@
+<!doctype html>
+<html lang="en" class="h-full bg-gray-100">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Laracasts: PHP for Beginners</title>
+    <script src="https://cdn.tailwindcss.com"></script>
+</head>
+<body class="h-full">
